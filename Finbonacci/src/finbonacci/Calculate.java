0fc60bcd4fe7c38dbd1c6830f02a12196d5bb91d
Trần Trung <nguyentrantrung1986@gmail.com>
2016-02-14 00:10:20 -0800

    define inputNumber method

---
+++ Finbonacci/src/finbonacci/Calculate.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finbonacci;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author TrungNT
+ */
+public class Calculate {
+    public void inputNumber(int[] a, int n)
+    {
+        try{
+            BufferedReader oBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            for(int i=0; i<n; i++)
+            {
+                System.out.print("a[" + i + "]=");
+                a[i] = Integer.parseInt(oBufferedReader.readLine());
+            }
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+}
---
+++ Finbonacci/src/finbonacci/Finbonacci.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finbonacci;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author TrungNT
+ */
+public class Finbonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] a = new int[100];
+        int n;
+        
+        try{
+            BufferedReader oBufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("\nn=");
+            n = Integer.parseInt(oBufferedReader.readLine());
+            Calculate oCalculate = new Calculate();
+            oCalculate.inputNumber(a, n);
+            
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    
+}
